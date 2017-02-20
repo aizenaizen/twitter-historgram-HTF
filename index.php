f25dbaa64353680a9d47a10b2037855190cce852
aizenaizen <josepeisen654@gmail.com>
2017-02-20 16:54:39 +0800

    v1.1

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+// TWITTER HISTOGRAM TEST FOR hardtofind
+// DATE: 20-FEB-17 
+// AUTHOR: Josep Eisen Montellano a.k.a. Eisen Grimbourne ;)
+
+// FINISHED @ 
+
+/*
+When creating a new twitter app, Callback URL must be set or else the application is locked to OOB mode and will not be able to use dynamic callbacks
 */
 define('TWITTER_OAUTH_CONSUMER_KEY', 'VqNBgc0Ej0WJyptAcfEgVfKcL');
 define('TWITTER_OAUTH_CONSUMER_SECRET', 'Sg9xpVrhabOGarwdmnknpa8i3qYrEZmrCMM92UT6VYhVYI4b2j');
@@ -19,7 +27,7 @@ $cb->setToken(
   TWITTER_OAUTH_TOKEN_SECRET
 );
 
-define('TWITTER_TIMELINE_USER', 'pewdiepie'); 
+define('TWITTER_TIMELINE_USER', 'pewdiepie'); // Because pewdiepie is currently on a hot seat, we shall use his account. :D
 
 // load the tweets
 $tweets = $cb->statuses_userTimeline([
